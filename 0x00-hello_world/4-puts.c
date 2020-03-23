79aa0f45fd086caa9163c6e1175f6541b030d3d6
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-03-23 20:26:33 +0000

    [4. Hello, puts] Added script that prints a phrase in the STDOUT

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point function
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	char output[52];
+	strcpy(output, "\"Programming is like building a multilingual puzzle\n");
+
+	puts(output);
+
+	return (0);
+}
