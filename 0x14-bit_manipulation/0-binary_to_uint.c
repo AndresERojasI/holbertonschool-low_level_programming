411d373f83dce456f17cba977ae6cfbdeae3c2da
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-03-26 04:29:00 +0000

    Moved the power function to the 0-binary_to_uint file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,3 +22,17 @@ unsigned int binary_to_uint(const char *b)
 
 	return (number);
 }
+
+int power(int base, int exponent)
+{
+	float temp;
+	if(exponent < 0) return (0);
+	if(exponent == 0) return (1);
+
+	temp = power(base, exponent/2);
+
+	if ((exponent % 2) == 0)
+		return (temp * temp);
+	else
+		return (base * temp * temp);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,5 @@
 #ifndef HOLBERTON_FLAG
 #define HOLBERTON_FLAG
-#include "power.c"
 
 int power(int base, int exponent);
 unsigned int binary_to_uint(const char *b);
