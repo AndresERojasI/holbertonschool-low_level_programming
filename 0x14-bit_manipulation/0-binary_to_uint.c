a69f8cbb44012c0b9df41d7113b41648d3db019d
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-03-26 05:15:04 +0000

    Removed strlen and improved readability

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 #include "holberton.h"
 
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int value;
-	unsigned int str_length;
+	int value;
+	int str_length;
 	unsigned int number;
 
 	if(b == NULL) return (0);
 
-	str_length = strlen(b);
-	number = 0;
+	str_length = 0;
 
+	while (b[str_length] != '\0')
+		str_length++;
+
+	number = 0;
 	for (i = str_length; i >= 0; i--){
 		if(b[i] == 0) continue;
 		if(b[i] != 48 && b[i] != 49) return (0);
