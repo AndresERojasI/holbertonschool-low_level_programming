4a838881f15fbf824fc5633d632a6da6550266e5
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-03-29 07:13:27 +0000

    Added extra validations for non-digit characters after first digit in argument

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int power(int base, int exponent);
 
 int main(int argc, char **argv)
 {
-	int j;
+	int j, i;
 	int result;
-	int temporal;
+	int units;
 
 	if(argc == 1) {
 		printf("0\n");
@@ -14,16 +17,33 @@ int main(int argc, char **argv)
 
 	result = 0;
 	for (j = 1; j < argc; ++j) {
-		temporal = atoi(argv[j]);
-		if(temporal != 0) {
-			result += temporal;
-		} else {
-			printf("Error\n");
-			return (0);
+		units = 0;
+		for (i = (strlen(argv[j]) -1); i >= 0 ; --i) {
+			if(argv[j][i] >= 48 && argv[j][i] <= 57) {
+				result += (argv[j][i] - 48) * power(10, units);
+				++units;
+			} else {
+				printf("Error\n");
+				return (0);
+			}
 		}
 	}
 
 	printf("%d\n", result);
 
 	return (0);
+}
+
+int power(int base, int exponent)
+{
+	float temp;
+	if(exponent < 0) return (0);
+	if(exponent == 0) return (1);
+
+	temp = power(base, exponent/2);
+
+	if ((exponent % 2) == 0)
+		return (temp * temp);
+	else
+		return (base * temp * temp);
 }
