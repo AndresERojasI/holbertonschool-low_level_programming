b368d164adeb88cf7a667515307a7d9594c43767
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-03-30 04:30:52 +0000

    Fixed memory leak when cannot allocate rows

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,8 +7,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **new_array;
 
-	if(width <= 0) return (NULL);
-	if(height <= 0) return (NULL);
+	if(width <= 0 || height <= 0) return (NULL);
 
 	new_array = (int **)malloc(height * sizeof(int *));
 
@@ -16,7 +15,12 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++) {
 		new_array[i] = (int *) malloc(width * sizeof(int));
-		if(new_array[i] == NULL) return (NULL);
+		if(new_array[i] == NULL) {
+			for (i = 0; i < height; i++) {
+				free(new_array[i]);
+				return (NULL);
+			}
+		};
 	}
 
 	for (i = 0; i <  height; i++)
