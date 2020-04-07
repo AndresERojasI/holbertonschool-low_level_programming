956ab48f7cb495c4b4a6c8cd9e2b7f532d8d5ca8
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-04-07 20:38:03 +0000

    Added base file for advanced Malloc task (not stable)

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char **strtow(char *str);
+{
+	int i, j, k;
+	int **new_array;
+
+	if(width <= 0 || height <= 0) return (NULL);
+
+	new_array = (int **)malloc(height * sizeof(int *));
+
+	if(new_array == NULL) return (NULL);
+
+	for (i = 0; i < height; i++) {
+		new_array[i] = (int *) malloc(width * sizeof(int));
+		if(new_array[i] == NULL) {
+			for (k = 0; k < i; ++k) {
+				free(new_array[k]);
+			}
+			free(new_array);
+			return (NULL);
+		};
+
+		for (j = 0; j < width; j++)
+			new_array[i][j] = 0;
+	}
+
+	return (new_array);
+}
