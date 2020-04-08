13668f501218e2677c94c7eaf5b048aef4139197
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-04-08 07:08:22 +0000

    [1. Under the snow] Added script to create a new file with content

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+int create_file(const char *filename, char *text_content){
+	int file_descriptor;
+	int bytes_written;
+	int bytes_to_write;
+
+	if(filename == NULL) return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
+	if (file_descriptor < 0) {
+		/* Raised failure on purpose, to check if the file already existed */
+		if (errno == EEXIST) {
+			/* the file already existed, then open it without changing the permissions */
+			file_descriptor = open(filename, O_WRONLY | O_TRUNC);
+		}
+	}
+
+	if(text_content == NULL){
+		bytes_written = write(file_descriptor, "", 1);
+	} else {
+		bytes_to_write = get_buffer_size(text_content);
+		bytes_written = write(file_descriptor, text_content, bytes_to_write);
+	}
+
+	free(text_content);
+	close(file_descriptor);
+
+	if(bytes_written < 0) return (-1);
+
+	return (1);
+}
+
+int get_buffer_size(char *buffer)
+{
+	int counter;
+	counter = 0;
+
+	while(buffer[counter] != '\0')
+		counter++;
+
+	return counter;
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,10 +1,16 @@
 #ifndef HOLBIE_VAR
 #define HOLBIE_VAR
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int get_buffer_size(char *buffer);
+int create_file(const char *filename, char *text_content);
 
 #endif
